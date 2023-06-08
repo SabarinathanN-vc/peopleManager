7dcbe5cd7b7321a6708251279e2d2f0928d447f9
sabarinathan_vc <sabarinathan.nagarajan@verticurl.com>
2023-06-08 10:17:44 +0530

    Add some comments for better understanding of code

--- src/components/users-list.jsx
+++ src/components/users-list.jsx
@@ -1,21 +1,29 @@
 import React, { useState ,useEffect} from "react";
+// people mangement url is coming from constan file for future changing of url(esasy to maintain) 
 import PeopleManagementURL from "../utils/constant";
 import ReadData from "../utils/httpMthodHandling";
 import "./users-list.scss";
 
 const UserList = () => {
+    //In this useState we have getting the array of values from the response
     const[userDetails ,setUserDetails] =useState([])
 
+    //here we use the useEffect for unwanted data re-rendering 
     useEffect(()=>{
+        // here we calling the ReadData function , and then we pass argument to the httpmethodHandling
         ReadData(PeopleManagementURL.USER_DETAILS,setUserDetails)
         .then(res=>{
            setUserDetails(res.data.users)
         })
-    },[])
+    },[])//---> here there we apply any dependencies on that time the useEffect will rerender
+
+    // in that we have two functions one is an imediate one time execution and anoter one is based on changes occures in the dependencies 
+    // it will rerender.
 
     return ( <div>
         <h1 className="title-bar">USER DETAILS</h1>
         <div className="cardContainer-wrapper">
+            {/* here we using map function to get array of individual datas ,here we creating a template for the card list to render the data to particular area  */}
         {userDetails.map((res)=>{
             return (
                 <div className="card-container">
